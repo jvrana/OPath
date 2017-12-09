6e1544623bd0ff2635a9d5664b9996b197ecca29
jvrana <justin.vrana@gmail.com>
2017-12-09 12:20:42 -0800

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __author__ = 'Justin Dane Vrana'
 __license__ = 'MIT'
 __package__ = "magicdir"
 __readme__ = "README"
-__version__ = "0.3.0a"
+__version__ = "0.3a"
 
 tests_require = [
     'pytest',
